55738b5db804c6a0cd8d84d94a6c79fb31b4a777
Luca <s0narr@protonmail.com>
2024-11-02 09:46:36 +0200

    Added server boilerplate, code skeleton for user (routes/model)

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+/* Express boilerplate; */
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv'); 
+
+const app = express();
+const PORT = 8080 || process.env.PORT;
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    console.log('Test');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
