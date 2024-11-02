1c974771bab8373c0d692b3546896ef3453589d6
Luca <s0narr@protonmail.com>
2024-11-02 12:15:43 +0200

    Added nodemon + npm start script to server

--- backend/user/routes/user.js
+++ backend/user/routes/user.js
@@ -2,8 +2,8 @@ const { Router, Request, Response } = require('express');
 const { verifyToken, registerUser, loginUser } = require('../controllers/user');
 const router = Router(); 
 
-router.post('/register', (req, res) => { registerUser()});
+router.post('/register', registerUser);
 
-router.post('/login', verifyToken, (req, res) => { loginUser()});
+router.post('/login', verifyToken, loginUser);
 
 module.exports = router;
