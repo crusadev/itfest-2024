f9342bbee43feca0adb0e1820d32739f8a1dd1ca
Luca <s0narr@protonmail.com>
2024-11-02 11:42:24 +0200

    Small user-route change

--- backend/user/routes/user.js
+++ backend/user/routes/user.js
@@ -1,7 +1,7 @@
 const { Router, Request, Response } = require('express');
-
+const { verifyToken, registerUser, loginUser } = require('../controllers/user');
 const Router = router(); 
 
-Router.post('/register', (req, res) => {
-    
-})
+Router.post('/register', (req, res) => { registerUser()});
+
+Router.post('/login', verifyToken, (req, res) => { loginUser()});
